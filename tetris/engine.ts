ea703820fc9184965cb8d4f28eb7499859c09d11
carlesba <carlesba@users.noreply.github.com>
2024-04-28 22:25:43 +0200

    reimplement ticker

--- tetris/engine.ts
+++ tetris/engine.ts
@@ -1,15 +1,13 @@
 import { Free } from "./Free";
 import { Maybe } from "./Maybe";
 import { Actions } from "./actions";
-import { Always, Apply, Identity } from "./functional";
+import { Identity } from "./functional";
 import { Game } from "./game";
 
 type TetrisEvent = {
   type: "gameover" | "scored" | "scoring" | "nextpiece" | "tick" | "action";
   game: Game;
 };
-
-type DispatchTetrisEvent = (event: TetrisEvent) => unknown;
 
 const SIZE = { row: 20, col: 10 };
 
@@ -20,108 +18,108 @@ const createGame = () =>
     .map((create) => create(Actions.randomTetrimo()))
     .run();
 
-class Timer {
-  date = 0;
-  static create() {
-    return new Timer();
+function nextGame(game: Game) {
+  switch (game.status) {
+    case "playing":
+      return Free.of(game).map(Actions.nextTick).run();
+    case "scoring":
+      return Free.of(game).map(Actions.score).run();
+    case "scored":
+      return Free.of(game)
+        .map(Actions.cleanupScore(Actions.randomTetrimo))
+        .run();
+    default:
+      return game;
   }
-  next = (gap: number) => {
-    this.date = gap + new Date().getTime();
-    return this;
+}
+
+function delayFromGame(game: Game): Maybe<number> {
+  switch (game.status) {
+    case "scoring": {
+      return Maybe.of(2000);
+    }
+    case "playing": {
+      return Maybe.of(game).map(Actions.levelSpeed);
+    }
+    case "scored": {
+      return Maybe.of(0);
+    }
+    default: {
+      return Maybe.none();
+    }
+  }
+}
+
+function createState(
+  dispatch: (event: {
+    type: "next" | "update" | "reset";
+    game: Game;
+    nextTick: () => unknown;
+  }) => unknown
+) {
+  let game = createGame();
+  const nextTick = () => {
+    game = nextGame(game);
+    dispatch({ type: "next", game, nextTick: nextTick });
   };
-  done = () => {
-    return new Date().getTime() > this.date;
+  return {
+    get: () => game,
+    update: (fn: (game: Game) => Game) => {
+      game = fn(game);
+      dispatch({ type: "update", game, nextTick: nextTick });
+    },
+    nextTick,
+    reset: () => {
+      game = createGame();
+      dispatch({ type: "reset", game, nextTick: nextTick });
+    },
   };
 }
 
-const CommandCreator = {
-  nextTick:
-    (timer: Timer) => (dispatch: DispatchTetrisEvent) => (value: Game) =>
-      Maybe.of(value)
-        .check(timer.done)
-        .map(Actions.nextTick)
-        .whenSome((game) =>
-          dispatch({
-            type: game.status === "scoring" ? "scoring" : "tick",
-            game,
-          })
-        )
-        .whenSome((game) =>
-          Free.of(value)
-            .map(Actions.levelSpeed)
-            .map((speed) => (game.scoringLines.length > 0 ? speed * 3 : speed))
-            .map(timer.next)
-            .run()
-        ),
-
-  scoring: (timer: Timer) => (dispatch: DispatchTetrisEvent) => (value: Game) =>
-    Maybe.of(value)
-      .check(timer.done)
-      .map(Actions.score)
-      .whenSome((game) => dispatch({ type: "scored", game })),
-
-  scored: (timer: Timer) => (dispatch: DispatchTetrisEvent) => (value: Game) =>
-    Maybe.of(value)
-      .check(timer.done)
-      .map(Actions.cleanupScore(Actions.randomTetrimo))
-      .whenSome((game) => dispatch({ type: "nextpiece", game })),
-};
+function delayedExecution(fn: () => unknown) {
+  let timer: NodeJS.Timeout;
+  return {
+    after: (delay: number) => {
+      timer = setTimeout(fn, delay);
+    },
+  };
+}
 
 export function Tetris(onEvent: (event: TetrisEvent) => unknown) {
-  let game = createGame();
+  const state = createState((event) => {
+    const type = Free.of(event.type)
+      .map((t) => {
+        switch (t) {
+          case "next":
+            return "tick";
+          case "reset":
+            return "tick";
+          case "update":
+          default:
+            return "action";
+        }
+      })
+      .run();
 
-  const timer = Timer.create();
-  const [nextTick, scoring, scored] = Object.values(CommandCreator)
-    .map((createCommand) => createCommand(timer))
-    .map((createCommand) => createCommand(onEvent));
+    onEvent({ type, game: event.game });
 
-  const nextGame = (game: Game) => {
-    switch (game.status) {
-      case "playing":
-        return nextTick(game);
-      case "scoring":
-        return scoring(game);
-      case "scored":
-        return scored(game);
-      default:
-        return Maybe.of(game);
+    const nextTick = delayedExecution(event.nextTick);
+    switch (event.type) {
+      case "next":
+        return delayFromGame(event.game).whenSome(nextTick.after);
+      case "reset":
+        return nextTick.after(0);
+      case "update":
+        return;
     }
-  };
-
-  // function* gametick() {
-  //   while (game.status !== "gameover") {
-  //     game = yield nextGame(game).fold({
-  //       onSome: Identity,
-  //       onNone: Always(game),
-  //     });
-  //   }
-  //   return game;
-  // }
+  });
 
   return {
     start() {
-      // const tick = gametick();
-      // let result = tick.next();
-      const interval = setInterval(() => {
-        game = nextGame(game).fold({
-          onSome: Identity,
-          onNone: Always(game),
-        });
-        if (game.status === "gameover") {
-          onEvent({ type: "gameover", game });
-          clearInterval(interval);
-        }
-      }, 10);
-      // result.value;
-      // while (!result.done) {
-      //   requestAnimationFrame(() => {
-      //     result = tick.next();
-      //   });
-      // }
-      onEvent({ type: "gameover", game });
+      state.reset();
     },
     action(action: "left" | "right" | "down" | "rotateA" | "rotateB") {
-      game = Free.of(action)
+      Free.of(action)
         .map((action) => {
           switch (action) {
             case "left":
@@ -138,9 +136,8 @@ export function Tetris(onEvent: (event: TetrisEvent) => unknown) {
               return Identity;
           }
         })
-        .map(Apply(game))
+        .map(state.update)
         .run();
-      onEvent({ type: "action", game });
     },
   };
 }
