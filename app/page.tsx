d1844e11fce4c109b67ab3285c13494eca289147
carlesba <carlesba@users.noreply.github.com>
2025-05-02 18:25:48 +0200

    games by props

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 import { GameBoy } from "@/gameboy-react";
 import { TetrisCartridge } from "@/tetris-react";
 
+const GAMES = [{ name: "Tetris", Cartridge: TetrisCartridge }];
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <GameBoy Cartridge={TetrisCartridge} />
+      <GameBoy games={GAMES} />
     </main>
   );
 }
--- gameboy-react/Gameboy.tsx
+++ gameboy-react/Gameboy.tsx
@@ -6,31 +6,33 @@ import { CartridgeComponent, ControlEventsProvider } from "@/cartridge-react";
 import { ControlEventsObservable, Speaker, createSpeaker } from "@/cartridge";
 import { AudioProvider, BootCartridge } from "@/cartridge-react";
 
-export function GameBoy(props: { Cartridge: CartridgeComponent }) {
+export function GameBoy(props: {
+  games: Array<{ name: string; Cartridge: CartridgeComponent }>;
+}) {
   const [controlEvents] = useState(() => new ControlEventsObservable());
   const [speaker, setSpeaker] = useState<Speaker | null>(null);
 
   useKeyboardControls(controlEvents.dispatch);
-  const [cartridge, setCartridge] = useState<"boot" | "tetris">("boot");
-
-  const screen =
-    cartridge === "boot" ? (
-      <BootCartridge
-        onClose={() => {
-          setCartridge("tetris");
-          setSpeaker(createSpeaker());
-        }}
-      />
-    ) : (
-      <props.Cartridge onClose={() => setCartridge("boot")} />
-    );
+  const [index, setIndex] = useState(-1);
+  const Game = props.games[index]?.Cartridge;
 
   return (
     <AudioProvider value={speaker}>
       <ControlEventsProvider controlEvents={controlEvents}>
         <Layout
           controls={<Controls onAction={controlEvents.dispatch} />}
-          screen={screen}
+          screen={
+            !Game ? (
+              <BootCartridge
+                onClose={() => {
+                  setIndex(0);
+                  setSpeaker(createSpeaker());
+                }}
+              />
+            ) : (
+              <Game onClose={() => setIndex(-1)} />
+            )
+          }
         />
       </ControlEventsProvider>
     </AudioProvider>
